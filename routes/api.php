68d3a9133883fc2af5e81fbd3f282af2df5a51a2
pejanamillard <pejanamillard@gmail.com>
2026-03-05 23:12:54 +0800

    Complete Activity 3: Authentication with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,37 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
+// Public Login Route
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-Route::get('/user', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    // Creates the token for the user
+    $token = $user->createToken('myapptoken')->plainTextToken;
+
     return response()->json([
-        'message' => 'Hello World! Ako diay si Millard FIl L. Pejana',
-        'data' => null
+        'user' => $user,
+        'token' => $token
+    ], 201);
+});
+
+// Protected Route (Requires Token)
+Route::middleware('auth:sanctum')->get('/check-auth', function (Request $request) {
+    return response()->json([
+        'message' => 'Authentication Successful!',
+        'user' => $request->user()
     ]);
 });
